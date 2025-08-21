7d94ad30fe46d36d06440910c5957826662b23a8
RaihanKabir277 <raihank0198@gmail.com>
2025-08-21 21:19:47 +0600

    completed the project with added smtplib for transfer message

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import datetime as dt
 import pandas
 import random
+import smtplib
+
+MY_EMAIL = "........@gmail.com"
 # now = dt.datetime.now()
 # day_of_week = now.weekday()
 # month = now.month()
@@ -14,12 +17,16 @@ today = (dt.datetime.now().month, dt.datetime.now().day)
 data = pandas.read_csv("birthdays.csv")
 
 birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
-
+print(birthdays_dict)
 if today in birthdays_dict:
     person = birthdays_dict[today]
     file_path = f"letter_templates/letter_{random.randint(1,4)}.txt"
     with open(file_path) as file:
         contents = file.read()
-        contents.replace("[NAME]", person["name"])
-    print(contents)
+        contents = contents.replace("[NAME]", person["name"])
+    
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, password="*********")
+        connection.send_message(from_addr=MY_EMAIL, to_addrs=person["email"], msg=f"Subject: Happy Birthday To You! \n\n{contents}")
 
