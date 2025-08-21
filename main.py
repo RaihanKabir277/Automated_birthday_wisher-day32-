356d8e6e9b4cf87f3141f8c2982cae729fac9dff
RaihanKabir277 <raihank0198@gmail.com>
2025-08-21 18:39:41 +0600

    read the csv file , dictionary comprehension, and open a random txt file added here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime as dt
 import pandas
-
+import random
 # now = dt.datetime.now()
 # day_of_week = now.weekday()
 # month = now.month()
@@ -9,7 +9,17 @@ import pandas
 
 # ------- easier way to do the same things --------
 
-today = (dt.datetime.now().month, dt.datetime.now().weekday)
+today = (dt.datetime.now().month, dt.datetime.now().day)
 
 data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,4)}.txt"
+    with open(file_path) as file:
+        contents = file.read()
+        contents.replace("[NAME]", person["name"])
+    print(contents)
 
